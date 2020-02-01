bd04ab66b2a9a73ec967d3a2eeab87b61e284efa
Goutham <gouthams0922@gmail.com>
2020-02-01 22:50:15 +0530

    Mark Attendence and Update Attendence was completed by gokul
    Signed-off-by: Goutham <gouthams0922@gmail.com>

---
+++ app/src/main/java/com/example/android/smartattendence/MainUpdate.java
@@ -0,0 +1,41 @@
+package com.example.android.smartattendence;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainUpdate extends AppCompatActivity implements Connector {
+
+    String classes[] = {"Class A", "Class B", "Class C"};
+    ArrayList<String> list = new ArrayList<String>(Arrays.asList("111,222,333,444,555,666".split(",")));
+    ArrayList<String> list1 = new ArrayList<String>(Arrays.asList("true,true,false,true,false,false".split(",")));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_update);
+        Toast.makeText(MainUpdate.this, list1.get(1), Toast.LENGTH_LONG).show();
+
+        ListView listView = (ListView) findViewById(R.id.list1);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter adap1 = new ArrayAdapter<String>(getApplicationContext(), R.layout.spinner_class, classes);
+        adap1.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner.setAdapter(adap1);
+        listView.setAdapter(new MyCustomAdapter(list, list1, MainUpdate.this, MainUpdate.this));
+
+
+    }
+
+    @Override
+    public void onCheckedBox(String s, int pos) {
+        Toast.makeText(MainUpdate.this, s + pos, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/example/android/smartattendence/MenuActivity.java
+++ app/src/main/java/com/example/android/smartattendence/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.smartattendence;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Button button_mark = (Button) findViewById(R.id.button_mark);
+        button_mark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuActivity.this, MainMark.class);
+                startActivity(i);
+            }
+        });
+        Button button_update = (Button) findViewById(R.id.button_update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuActivity.this, MainUpdate.class);
+                startActivity(i);
+            }
+        });
     }
 }
